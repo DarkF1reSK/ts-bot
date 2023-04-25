e03047a32f7b33cc1b24d60804a4be98ebecec85
DarkF1reSK <realdarkf1resk@gmail.com>
2023-04-25 21:41:17 +0200

    lyrics hopefully

--- src/commands/music/lyrics.ts
+++ src/commands/music/lyrics.ts
@@ -1,10 +1,14 @@
 import {CommandObject, CommandType} from "wokcommands";
-import {ApplicationCommandOptionType, EmbedBuilder} from "discord.js";
-const { lyricsExtractor } = require("@discord-player/extractor")
+import {ApplicationCommandOptionType, Embed, EmbedBuilder} from "discord.js";
 import player from "../../utils/Player";
+import { lyricsExtractor } from "@discord-player/extractor";
+import {Client} from "genius-lyrics"
+import {geniusKey} from "../../../config.json"
 
 
 const search = lyricsExtractor()
+
+const lyricsClient = new Client(geniusKey)
 
 export default {
     description: "Lyrics",
@@ -19,11 +23,13 @@ export default {
         }
     ],
     callback: async ({interaction}) => {
-        try{
+        //use this if it works
+        /*try{
             await interaction.deferReply("working")
 
             const queue = player.nodes.get(interaction.guild)
             const music = interaction.options.getString("name")
+            console.log(music)
             if(!queue && !music) {
                 return interaction.editReply({ content: "There is no queue and you didn't mention any song name!"})
             }
@@ -31,11 +37,15 @@ export default {
             if (queue || music) {
                 const result = await search.search(music ?? queue.currentTrack.title)
 
+                const firstSong = result[0]
+
                 if (!result) {
                     return interaction.editReply({ content: `No lyrics found for: ${music ? music : queue.currentTrack.title}` })
                 }
+                const lyrics = await firstSong.lyrics();
 
-                const trimmedLyrics = result.lyrics.substring(0, 1997);
+                const trimmedLyrics = lyrics.substring(0, 1997);
+                console.log(trimmedLyrics)
 
                 const embed = new EmbedBuilder()
                     .setTitle(`${result.title}`)
@@ -48,7 +58,31 @@ export default {
                 interaction.editReply({ content: "Could not parse lyrics"})
             }
             console.log(error)
-            return "Error"
+        }*/
+
+        try {
+
+            await interaction.deferReply()
+            const music = interaction.options.getString("name")
+
+
+            const searches = await lyricsClient.songs.search(music);
+            const firstSong = searches[0]
+
+            const lyrics = await firstSong.lyrics()
+            const trimmedLyrics = lyrics.substring(0, 4000)
+
+            const embed = new EmbedBuilder()
+                .setTitle(firstSong.artist.name)
+                .setThumbnail(firstSong.thumbnail)
+                .setDescription(trimmedLyrics)
+                .setTimestamp()
+            interaction.editReply({ embeds: [embed] })
+
+        }
+        catch (err) {
+            interaction.editReply(err)
+
         }
     }
 } as CommandObject
